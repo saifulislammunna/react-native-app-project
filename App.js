e35bc621d709d936fbfaa40d74b3f1b331bf2ea4
saifulislammunna <saifulmunna28@gmail.com>
2022-06-02 16:56:48 +0600

    image-added

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <View style={{height: 30,  backgroundColor:"red"}}>
-       <Text>This is a text</Text>
+      <View style={{height: 60,
+          backgroundColor:"blue"}}>
+       <Text style={{color: "white" }}>This is a text</Text>
        </View>
+      <View style={{height: 60,  backgroundColor:"green"}}>
+       <Text style={{fontsize: 24,
+         color: "white",
+         fontWeight: "bold",
+         padding: 10}}>This is another text</Text>
+       </View>
+       <View style={{marginTop: 50, marginLeft: 40}}>
+       <Image style={{height: 180, width: 200}} 
+       source={require('./assets/favicon.png')}
+       resizeMode="contain"
+       />
+       
+       </View>
+       <View style={{marginTop: 50, marginLeft: 40}}>
+       <Image style={{height: 180, width: 200}} 
+       source={{uri: "https://i.ibb.co/q9wzfnD/hilux.jpg"}}
+       resizeMode="contain"
+       />
+       
+       </View>
+
+    
     </SafeAreaView>
   );
 }
