ce84bcb2eb031b08b68745400155fcdd75c9a637
saifulislammunna <saifulmunna28@gmail.com>
2022-06-02 20:37:44 +0600

    added

--- App.js
+++ App.js
@@ -63,13 +63,16 @@ The pickup truck was sold with the HiLux name in most markets, but in North Amer
       <Button title="press"/>
       <Button title="2nd title"/> */}
       <View style={{flex: 1,
-      flexDirection: "column", // main axis -> column
-      alignItems: "center", //it works cross axis
-      justifyContent: "center",
+      flexDirection: "row", // main axis -> column
+      // alignItems: "center", //it works cross axis
+      // justifyContent: "center",
+      flexWrap: "wrap"
        }}>
       <View style={styles.view1}/>
       <View style={styles.view2}/>
       <View style={styles.view3}/>
+      <View style={styles.view4}/>
+      <View style={styles.view5}/>
       </View>
       
     </SafeAreaView>
@@ -87,12 +90,25 @@ const styles = StyleSheet.create({
       height: 100,
       width: 100,
       backgroundColor: 'blue',
-      alignSelf: "flex-start"
+      alignSelf: "flex-start",
+      // marginHorizontal: 20,
      },
      view3: {
       height: 100,
       width: 100,
        backgroundColor: 'green'
+     }
+  ,
+     view4: {
+      height: 100,
+      width: 100,
+       backgroundColor: 'orange'
+     }
+  ,
+     view5: {
+      height: 100,
+      width: 100,
+       backgroundColor: 'purple'
      }
   ,
 });
